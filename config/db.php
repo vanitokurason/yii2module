556a8323af4070e9d0376d7d1aebf6b07540718c
vanito-kurason <vanitocurason@gmail.com>
2022-11-14 08:53:10 +0300

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=pp_test',
+    'username' => 'Kurason',
+    'password' => 'Kiparis_88',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
+];
